f67e93da0fe0a6ec09c7f57ca20d4c424ab4e2e4
Alex Leitch <aleitch1@umd.edu>
2021-11-18 10:33:18 -0500

    Including first pass at react hooks

--- src/components/example.js
+++ src/components/example.js
@@ -1,4 +1,6 @@
 
+import React, {useState, useEffect} from "react";
+
 /*
 * Here is an example component, the props (children, number, text) being used are
 * destructured in this example but do not have to be. Note that javascript within
@@ -10,16 +12,28 @@ export default function Example({
     number,
     text
 }) {
-    const newNumber = number * number
-    let newText
-
+    const [newNumber, setNewNumber] = useState(0);
+    
+    let newText;
     if (text) {
         newText = text.toUpperCase()
     }
 
+    // Similar to componentDidMount and componentDidUpdate:
+  useEffect(() => {
+    async function fetchData() {
+        // Update the document title using the browser API
+        const request = await fetch('/api/');
+        const json = await request.json();
+        console.log(json)
+    }
+    fetchData();
+  });
+
+
     return (
         <div>
-            <p>This is your new number: {newNumber}</p>
+            <p>This is your new number: {}</p>
             <p>This is your new text: {newText}</p>
             {children}
         </div>
