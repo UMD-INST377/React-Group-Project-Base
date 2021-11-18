e6847cc42a236b807970774997c011efda9b6236
Alex Leitch <aleitch1@umd.edu>
2021-11-18 12:13:32 -0500

    Add fetchExample to app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Link } from 'react-router-dom';
 import './App.css';
 import Example from './components/example.js';
 import HooksExample from './components/hooksexample.js';
+import FetchExample from './components/fetchexample';
 
 /*
  * This will be your index page regardless of whether you are using 1
@@ -12,9 +13,10 @@ function App() {
     <div className="App">
       <h1>This is the first page!</h1>
       <Link to='/secondpage'>Second Page</Link>
-      <Example number={8} text='this is text passed through as a prop!'>
+      {/* <Example number={8} text='this is text passed through as a prop!'>
         <p>This is a child of &lt;Example&gt;.</p>
-      </Example>
+      </Example> */}
+      <FetchExample />
       <HooksExample />
     </div>
   );
